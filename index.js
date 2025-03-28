726afe89b234153fa07b7832b640badd2d999079
Bright Obiri Gyan <74192474+Bbrightr@users.noreply.github.com>
2025-03-28 21:41:03 +0100

    refractoring 1

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
-import { propertyForSaleArr } from "./properties/propertyForSaleArr.js"
+import placeholderPropertyObj from "properties/propertyForSaleArr"
+// import propertyForSaleArr from "properties/placeholderPropertyObj"
+
 console.log(propertyForSaleArr)
+// console.log(placeholderPropertyObj)
 
 function getPropertyHtml(properties) {
+    return properties.map(property => {
+        const {
+            propertyLocation,
+            priceGBP,
+            roomsM2,
+            comment,
+            image,
+        } = property
+
+        const totalRoom = roomsM2.reduce((total, currentElement)=> total + currentElement)
+
+        return `<section class="card">
+                <img src="/images/${image}">
+                <div class="card-right">
+                    <h2>${propertyLocation}</h2>
+                    <h3>${priceGBP}</h3>
+                    <p>${comment}</p>
+                    <h3>${totalRoom} m&sup2;</h3>
+                </div>
+            </section> `
+    })
     
-    if (properties === 0){
-        return `<p>There are no properties listed here</p>`
-    } else {
-
-        const houseListing = properties.map(function(property){
-            const totalSize = property.roomsM2.reduce(function(total, currentValue = 0){
-                return total + currentValue
-            })
-
-            return `  <section class="card">
-                    <img src="/images/${property.image}">
-                    <div class="card-right">
-                        <h2>${property.propertyLocation}</h2>
-                        <h3>${property.priceGBP} GBP</h3>
-                        <p>${property.comment}</p>
-                        <h3>${totalSize} m&sup2;</h3>
-                    </div>
-                </section>   `
-        } ,0).join(' ')
-
-        return houseListing
-    }
     }
     
     document.getElementById('container').innerHTML = getPropertyHtml(propertyForSaleArr)
--- properties/propertyForSaleArr.js
+++ properties/propertyForSaleArr.js
@@ -29,4 +29,4 @@ export const propertyForSaleArr = [
     }
 ]
 
-
+// export default propertyForSaleArr
